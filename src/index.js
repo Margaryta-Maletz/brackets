b111582bc9470f4b436820f40b64b156d01255e5
github-@Маргарита#6359 <rita.maletz@gmail.com>
2021-03-10 08:39:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.split(''),
+        temp = [];
+    const trans = bracketsConfig[0].map((col, j) => bracketsConfig.map(row => row[j]));
+
+    for (let i = 0; i < arr.length; i++) {
+        
+        if (trans[0].includes(arr[i])) {
+            temp.push(arr[i]);
+        }
+
+        if (trans[1].includes(arr[i])) {
+            if ((temp.length !== 0) & (temp[temp.length-1] === trans[0][trans[1].indexOf(arr[i])])) {
+                temp.pop();
+            } else { 
+                return false; 
+            }
+        } 
+
+        if (trans[0].includes(arr[i]) & trans[1].includes(arr[i])) {
+            if (temp.length === 0 | (temp[temp.length-1] !== arr[i])) {
+                temp.push(arr[i]);
+            } else {
+                temp.pop();
+            }
+        }
+
+    }
+    if (temp.length > 0) return false;
+    return true;
 }
